0ce38280d040525477da1b7b8ada2b7f7b2a5845
NatashaB-randao <natashabrandao57@hotmail.com>
2023-03-31 15:02:24 -0300

    Variáveis e tipos de dados

--- Aula1.playground/Contents.swift
+++ Aula1.playground/Contents.swift
@@ -1,4 +1,25 @@
-import UIKit
+var name: String = "Natasha"
+var age: Int = 24
+print(name)
+print(age)
 
-var greeting = "Hello, playground"
-print(greeting)
+
+// INTERPOLAÇÃO -> COLOCAR VARIÁVEIS DENTRO DE UM TEXTO
+
+var nome: String = "Natasha"
+var idade: Int = 24
+print("O meu nome é \(nome) e eu tenho \(idade) anos")
+
+var coeficienteRendimento: Double = 9.5
+var possuiHistoricoReprovacao: Bool = false
+var turnoEmQueEstuda: Character = "D"
+
+
+
+// TYPE ALIAS -> VÁRIAS VARIÁVEIS QUE REPRESENTAM APENAS UMA COISA
+typealias Estudante = String
+var aluno1: Estudante = "João"
+var aluno2: Estudante = "Ana"
+
+
+
