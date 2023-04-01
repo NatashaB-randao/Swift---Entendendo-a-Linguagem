0f0ac3e323d5afd42af8109d596fce9f1dcdcaca
NatashaB-randao <natashabrandao57@hotmail.com>
2023-04-01 11:25:47 -0300

    Desafio

---
+++ Desafio.playground/Contents.swift
@@ -0,0 +1,15 @@
+// CRIE UMA "FICHA"
+
+let inicialNome: Character = "N"
+let nome: String = "Natasha"
+let apelido: String = "Nah"
+let idade: Int = 24
+let altura: Double = 1.67
+let gostaDeSwift: Bool = true
+
+print("Inicial do nome: \(inicialNome)")
+print("Nome: \(nome)")
+print("Apelido: \(apelido)")
+print("Idade: \(idade)")
+print("Altura: \(altura)")
+print("Gosta de Swift: \(gostaDeSwift)")
