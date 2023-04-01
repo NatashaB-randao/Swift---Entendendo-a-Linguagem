c17489ec17f45611db5d4d660fa3ed9b73571b8d
NatashaB-randao <natashabrandao57@hotmail.com>
2023-04-01 14:45:51 -0300

    Desafio

--- Desafio - Cálculo de Média.playground/Contents.swift
+++ Desafio - Cálculo de Média.playground/Contents.swift
@@ -1,3 +1,7 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let grade1 = 9.0
+let grade2 = 5.0
+let grade3 = 6.5
+let averageGrades = (grade1 + grade2 + grade3) / 3
+print("A média das notas é \(averageGrades)")
