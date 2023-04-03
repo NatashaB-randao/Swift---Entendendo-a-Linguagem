fc8f22e39196f29e4aacdf4893a63476eeeef09f
NatashaB-randao <natashabrandao57@hotmail.com>
2023-04-03 14:12:45 -0300

    Desafio

---
+++ Desafio - Operadores lógicos e relacionais.playground/Contents.swift
@@ -0,0 +1,10 @@
+// Faça um programa que controle a entrada de pessoas em uma festa.
+
+let idade = 15
+var rgEmMaos = true
+
+if idade >= 18 && rgEmMaos {
+  print("Pode entrar")
+} else {
+  print("Não pode entrar")
+}
