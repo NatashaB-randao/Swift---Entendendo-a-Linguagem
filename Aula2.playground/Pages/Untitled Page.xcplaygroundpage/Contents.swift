73d0228a8dcfa424588bfb8aadb8df4130eb4886
NatashaB-randao <natashabrandao57@hotmail.com>
2023-04-03 12:46:42 -0300

    Aula 2

--- Aula2.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Aula2.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,7 +1,34 @@
-//: [Previous](@previous)
-
-import Foundation
+/ OPERADORES RELACIONAIS E LÓGICOS
 
 var greeting = "Hello, playground"
+var pontuacao1: Double = 8.0
+var pontuacao2: Double = 10.0
 
-//: [Next](@next)
+
+// Realizar comparações utiliza-se OPERADORES RELACIONAIS
+
+pontuacao1 > pontuacao2
+pontuacao1 >= pontuacao2
+pontuacao1 < pontuacao2
+pontuacao1 <= pontuacao2
+pontuacao1 == pontuacao2
+pontuacao1 != pontuacao2
+
+
+var usuario1Venceu = pontuacao1 > pontuacao2
+var usuario2Venceu = pontuacao2 > pontuacao1
+
+
+var idade = 22
+var adulto = idade >= 18
+var possuiCNHValida = true
+
+// OPERADORES LÓGICOS
+// And = &&
+// Or = ||
+// Not = !
+
+//var podeDirigir = adulto && possuiCNHValida
+var podeDirigir = adulto || possuiCNHValida
+!adulto
+
