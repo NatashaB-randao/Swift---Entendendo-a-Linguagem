a185f767b9ad78908de38f13efde5ae9d05bc02e
NatashaB-randao <natashabrandao57@hotmail.com>
2023-04-03 12:45:07 -0300

    Aula 2

--- Aula2.playground/Contents.swift
+++ Aula2.playground/Contents.swift
@@ -1,3 +1,32 @@
-import UIKit
+// OPERADORES RELACIONAIS E LÓGICOS
 
-var greeting = "Hello, playground"
+var pontuacao1: Double = 8.0
+var pontuacao2: Double = 10.0
+
+
+// Realizar comparações utiliza-se OPERADORES RELACIONAIS
+
+pontuacao1 > pontuacao2
+pontuacao1 >= pontuacao2
+pontuacao1 < pontuacao2
+pontuacao1 <= pontuacao2
+pontuacao1 == pontuacao2
+pontuacao1 != pontuacao2
+
+
+var usuario1Venceu = pontuacao1 > pontuacao2
+var usuario2Venceu = pontuacao2 > pontuacao1
+
+
+var idade = 22
+var adulto = idade >= 18
+var possuiCNHValida = true
+
+// OPERADORES LÓGICOS
+// And = &&
+// Or = ||
+// Not = !
+
+//var podeDirigir = adulto && possuiCNHValida
+var podeDirigir = adulto || possuiCNHValida
+!adulto
