33368b430d18443e5d4b44893bedc226fa98f012
NatashaB-randao <natashabrandao57@hotmail.com>
2023-04-03 12:16:55 -0300

    Rename - Aula1_part2

--- Aula2.playground/Contents.swift
+++ Aula2.playground/Contents.swift
@@ -1,27 +1,3 @@
-// OPERAÇÕES ARITMÉTICAS E DE ATRIBUIÇÃO
+import UIKit
 
-10 + 2
-10 - 2
-8 * 2
-10 / 2
-5 / 2
-5.0 / 2
-Double (5) / 2
-5 % 2       //Módulo (%) resto da divisão de um número inteiro por outro número inteiro
-4 % 2       //Lê-se -> quatro módulo 2
-
-5 + 2 * 10
-(5 + 2) * 10
-
-var number1 = 10
-var number2 = 50
-var sum = number1 + number2
-print(sum)
-number1 = number1 + 2
-number1 *= 2        //operador de atribuição composto
-
-
-var name: String = "Natasha"
-var lastName:String = "Brandão"
-print(name + " " + lastName)                    //Concatenação de Strings
-print("Meu nome é: \(name) \(lastName)")        //Interpolação de Variáveis
+var greeting = "Hello, playground"
