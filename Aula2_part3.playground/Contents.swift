79e63378e80c34b8efb7d67067ebb14cc0af3d4f
NatashaB-randao <natashabrandao57@hotmail.com>
2023-04-03 13:38:51 -0300

    Aula2_part3

--- Aula2_part3.playground/Contents.swift
+++ Aula2_part3.playground/Contents.swift
@@ -1,3 +1,22 @@
-import UIKit
+// CONHECENDO INTERVALO DE VALORES E CONSICIONAIS COM SWITCH
 
-var greeting = "Hello, playground"
+// Intervalo de Valores -> Intervalo Fechado e Intervalo semi-aberto
+var intervaloFechado = 10...20      //significa que o número 20 está incluso no intervalo
+var intervaloSemiAberto = 10..<20   //significa que o número 20 não está incluso no intervalo
+
+/*var numero = 15
+
+switch numero {
+case 0...10: print("Número está entre 0 e 10")
+case 10...20: print("Número está entre 10 e 20")
+default: break
+}*/
+
+
+var numero = 25
+
+switch numero {
+case 0...10: print("Número está entre 0 e 10")
+case 10...20: print("Número está entre 10 e 20")
+default: (print("Número é maior do que 20"))
+}
