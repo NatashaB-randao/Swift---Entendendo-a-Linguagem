6a6e7dc1a3d7251a4403f1a872559cfde5813f7a
NatashaB-randao <natashabrandao57@hotmail.com>
2023-04-07 14:07:59 -0300

    Aula4_part3

--- Aula4_part3.playground/Contents.swift
+++ Aula4_part3.playground/Contents.swift
@@ -1,3 +1,21 @@
-import UIKit
+// ENTENDENDO OPCIONAIS EM SWIFT
 
-var greeting = "Hello, playground"
+var telefone: String?
+telefone = "999999"
+print(telefone)
+
+//Nil significa algo nulo, inexistente. portanto, a opcional pode ter um valor ou ela pode ser nil.
+
+
+
+
+
+
+
+
+
+
+
+/*var numeroEmString = "45"
+var numero = Int(numeroEmString)
+print(numero)*/
