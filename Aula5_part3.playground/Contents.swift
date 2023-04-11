998ae4ce3d82446e966333c1a4514bf79c2db864
NatashaB-randao <natashabrandao57@hotmail.com>
2023-04-11 13:34:35 -0300

    Aula5_part3

--- Aula5_part3.playground/Contents.swift
+++ Aula5_part3.playground/Contents.swift
@@ -1,3 +1,22 @@
-import UIKit
+// Conhecendo Tuplas
+// Pode ter valores e tipos diferentes
+// Normalmente, Tuplas representam um parde valores. Mas, também é possível adicionar mais valores
 
-var greeting = "Hello, playground"
+typealias Coordenadas = (x: Int, y: Double, z: Double)
+var coordenadas: Coordenadas = (x: 5, y: 3.5, z:8.0)
+coordenadas.x
+coordenadas.y
+coordenadas.z
+
+
+var pessoa = (nome: "Natasha", idade: 24)
+pessoa.nome
+pessoa.idade
+
+/*var(nome, idade) = pessoa
+print(nome)
+print(idade)*/
+
+
+var(_, idade) = pessoa
+print(idade)
