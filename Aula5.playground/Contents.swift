5c7b84817ab9698e97224a47cd05e077da77454c
NatashaB-randao <natashabrandao57@hotmail.com>
2023-04-11 12:28:40 -0300

    Conhecendo Arrays

---
+++ Aula5.playground/Contents.swift
@@ -0,0 +1,22 @@
+// Conhecendo arrays em Swift
+// Array -> coleção de valores
+
+
+var notas: [Double] = [8.5, 9.0, 7.2, 6.0]
+notas[0]
+
+
+var arrayVazio: [String] = []
+
+// ou
+var arrayVazio2 = Array<String>()
+
+notas.isEmpty
+notas.count
+notas.min()
+notas.max()
+notas.contains(8.5)
+notas.firstIndex(of: 9.0)
+
+
+// Aplicando métodos de array
