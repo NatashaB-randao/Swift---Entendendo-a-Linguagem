f07cc381abcafeb86fd57b860741d4d2daad31b0
NatashaB-randao <natashabrandao57@hotmail.com>
2023-04-11 12:56:27 -0300

    Aula5

--- Aula5.playground/Contents.swift
+++ Aula5.playground/Contents.swift
@@ -20,3 +20,54 @@ notas.firstIndex(of: 9.0)
 
 
 // Aplicando métodos de array
+notas.append(10)
+notas.insert(5.0, at: 2)
+notas += [8.2]
+print(notas)
+
+var elementoRemovido = notas.removeLast()
+print(elementoRemovido)
+
+print(notas)
+print("========")
+
+var elementoRemovido2 = notas.removeFirst()
+print(elementoRemovido2)
+
+print(notas)
+print("========")
+
+var elementoRemovido3 = notas.remove(at: 3)
+print(elementoRemovido3)
+
+print(notas)
+print("========")
+
+
+for nota in notas {
+    print(nota)
+}
+print("========")
+
+
+for i in 0..<notas.count {
+    print(notas[i])
+}
+print("========")
+
+for (index, elemento) in notas.enumerated() {
+    print(index, elemento)
+}
+
+
+
+print("========")
+
+let array1 = [Int]()
+let array2 = []
+let array3: [String] = []
+let array4 = [1, 2, 3]
+print(array4[0])
+array4.append(5)
+//Na linha 2, não há como criar um array vazio sem saber o tipo que os dados terão. Nesse caso, é necessário explicitar o tipo de dado.
+//Já a linha 06 causará erro pois o array foi inicializado como let, ou seja, como uma constante. Assim, não podemos realizar nenhuma operação de mudança nesse array.
